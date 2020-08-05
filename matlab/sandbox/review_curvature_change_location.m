a901c533358268cc29153d0e0c4e865f2832ad8f
jacheung <jacheung6@gmail.com>
2020-08-05 12:46:24 -0700

    updated 200805

--- matlab/sandbox/review_curvature_change_location.m
+++ matlab/sandbox/review_curvature_change_location.m
@@ -2,16 +2,23 @@
 load('C:\Users\jacheung\Dropbox\LocationCode\DataStructs\excitatory_all.mat') %L5b excitatory cells
 % load('C:\Users\jacheung\Dropbox\LocationCode\DataStructs\interneurons.mat') %L5b inhibitory cells
 U = struct_editor(U);
+
+hilbertVar = 'angle';
+% tStruct = object_location_quantification(U,touchCells,hilbertVar,'off');
+tStruct = object_location_quantification(U,1:length(U),hilbertVar,'off');
+
+loc_units = cellfun(@(x) x.is_tuned==1,tStruct);
 %%
 dk_corr = zeros(1,numel(U));
 dt_corr = zeros(1,numel(U));
 ccoeff = cell(1,numel(U));
 fr_diff = cell(1,numel(U)); 
+dk_peak_diff = nan(numel(U),2);
 
 num_curvature_bins = 2; 
 num_angle_bins = 8;
 
-for i = 1:length(U)
+for i = 86
     
     % raw variables
     touchOn = [find(U{i}.S_ctk(9,:,:)==1)  ;find(U{i}.S_ctk(12,:,:)==1)];
@@ -43,49 +50,113 @@ for i = 1:length(U)
         % response calculation
         response_window = U{i}.meta.touchProperties.responseWindow(1):U{i}.meta.touchProperties.responseWindow(2);
         tresponse = mean(spikes(touchOn + response_window),2) * 1000;
-    
-        % plotting
+        cmap_response = spikes(touchOn + [-25:50]) * 1000; 
+        
+        
         % protraction dk/dt correlation w/ pole location
         pt = dk<0;
         dk_corr(i) = corr(touch_poles(pt)',dk(pt));
         dt_corr(i) = corr(touch_poles(pt)',dt(pt));
-
+        
+        % filtering out retraction touches
         pt_spikes = tresponse(pt);
         pt_dk = dk(pt);
         pt_theta = touchTheta(pt);
+        pt_cmap = cmap_response(pt,:);
 
+        % plot 1 - 
         norm_pt = normalize_var(pt_spikes,1,0.1);
         [sort_colors,sort_idx] = sort(norm_pt,'descend');
         figure(03);clf
-        subplot(2,1,1)
+        subplot(3,2,[1 2])
         scatter(pt_theta(sort_idx),pt_dk(sort_idx),20,repmat(sort_colors,1,3),'filled');
         xlabel('whisker angle at touch')
         ylabel('max curvature')
         title('protraction touch analysis')
         
-        %stratification analyses (low and high curvature      
-        [dk_sorted, dksby] = binslin(pt_dk,[pt_theta pt_spikes],'equalN',num_curvature_bins);
+        %% plot 2 - stratification analyses (low and high curvature      
+%         [dk_sorted, dksby] = binslin(pt_dk,[pt_theta pt_spikes pt_cmap],'equalN',num_curvature_bins);
+%         
+%         sorted = cell(1,numel(dksby));
+%         subplot(3,2,[3 4])
+%         for p = 1:numel(dksby)
+%             [sorted{p}, sortedby] = binslin(dk_sorted{p}(:,1),dk_sorted{p}(:,2),'equalN',num_angle_bins,min(pt_theta),max(pt_theta));     
+%             hold on; plot(cellfun(@median, sortedby),cellfun(@mean,sorted{p}))
+%             
+%             vals = cellfun(@mean,sorted{p});
+%             [peak_fr, max_idx] = max(vals);
+%             avg_not_max = mean(vals(setdiff(1:length(vals),max_idx))); % mean of not max
+%             dk_peak_diff(i,p) = peak_fr - avg_not_max;
+%         end
+%         
+%         avg_curves = cell2mat(cellfun(@(x) cellfun(@mean, x),sorted,'uniformoutput',0));
+%         normed = normalize_var(avg_curves(:),0,1);
+%         norm_avg_curves = reshape(normed,num_angle_bins,2);
+%         ccoeff{i} = corr(avg_curves);
+%         fr_diff{i} = norm_avg_curves(:,1)-norm_avg_curves(:,2);
+%         
+%         xlabel('whisker angle at touch')
+%         ylabel('firing rate (spks/s)')
+%         legend('high dk','low dk')
+%         
+%         peak_val = zeros(1,2); 
+%         for p = 1:numel(dksby)
+%             [sorted{p}, sortedby] = binslin(dk_sorted{p}(:,1),dk_sorted{p}(:,3:end),'equalN',num_angle_bins,min(pt_theta),max(pt_theta));     
+%             subplot(3,2,4+p)
+%             strat_cmap = cell2mat(cellfun(@mean,sorted{p},'uniformoutput',0));
+%             peak_val(p) = max(strat_cmap(:)); 
+%             hold on; imagesc(strat_cmap)
+%             set(gca,'ytick',1:length(sortedby),'yticklabel',round(cellfun(@mean, sortedby)),...
+%             'ylim',[1 length(sortedby)], 'xtick',[0:25:75],'xticklabel',[-25:25:50])
+%             colorbar;
+%         end
+%         
+%         subplot(3,2,5); caxis([0 max(peak_val)])
+%         subplot(3,2,6); caxis([0 max(peak_val)])
+%         
+%         suptitle(['cell num ' num2str(i)])
+%% plot 3 - stratification analyses - location first then curvature change    
+
+        [dk_sorted, thetasby] = binslin(pt_theta,[pt_dk pt_spikes pt_cmap],'equalN',num_angle_bins,min(pt_theta),max(pt_theta));
         
-        sorted = cell(1,numel(dksby));
-        subplot(2,1,2)
-        for p = 1:numel(dksby)
-            [sorted{p}, sortedby] = binslin(dk_sorted{p}(:,1),dk_sorted{p}(:,2),'equalN',num_angle_bins,min(pt_theta),max(pt_theta));     
-            hold on; plot(cellfun(@median, sortedby),cellfun(@mean,sorted{p}))
+        sorted = cell(1,numel(thetasby));
+        sortedmap = cell(1,numel(thetasby));
+       
+        for p = 1:numel(thetasby)
+            [sorted{p}, sortedby] = binslin(dk_sorted{p}(:,1),dk_sorted{p}(:,2),'equalN',num_curvature_bins);
+            [sortedmap{p}, sortedby] = binslin(dk_sorted{p}(:,1),dk_sorted{p}(:,3:end),'equalN',num_curvature_bins);
         end
-        
-        avg_curves = cell2mat(cellfun(@(x) cellfun(@mean, x),sorted,'uniformoutput',0));
-        normed = normalize_var(avg_curves(:),0,1);
-        norm_avg_curves = reshape(normed,num_angle_bins,2);
-        ccoeff{i} = corr(avg_curves);
-        fr_diff{i} = norm_avg_curves(:,1)-norm_avg_curves(:,2);
-        
+        % plot of tuning curves
+        responses = cell2mat(cellfun(@(x) cellfun(@mean, x) ,sorted,'uniformoutput',0))';
+        stim =  cellfun(@median,thetasby);
+        subplot(3,2,[3 4])
+        plot(stim,responses)
         xlabel('whisker angle at touch')
         ylabel('firing rate (spks/s)')
         legend('high dk','low dk')
         
+        % plot heatmaps for different dk
+        maps = cell(1,num_curvature_bins);
+        for b = 1:numel(maps)
+            maps{b} = cell2mat(cellfun(@(x) mean(x{b}), sortedmap,'uniformoutput',0)');
+            subplot(3,2,4+b)
+            imagesc(imgaussfilt(maps{b},.5))
+            set(gca,'ytick',1:length(stim),'yticklabel',round(stim),...
+            'ylim',[1 length(stim)], 'xtick',[0:25:75],'xticklabel',[-25:25:50]...
+            ,'ydir','normal')
+            colorbar;
+            
+        end
+        suptitle(['cell num ' num2str(i)])
         if mean( cellfun(@numel,sorted{1})) < 20
             disp('warning: number of samples per angle bin < 20')
         end
+        
+        normed = normalize_var(responses(:),0,1);
+        norm_avg_curves = reshape(normed,num_angle_bins,2);
+        ccoeff{i} = corr(responses);
+        fr_diff{i} = norm_avg_curves(:,1)-norm_avg_curves(:,2);
+        
         
     catch
         touch_cells = cellfun(@(x) isfield(x.meta.touchProperties,'responseWindow'),U);
@@ -95,8 +166,25 @@ for i = 1:length(U)
         disp(['cell ' num2str(i) ' is not a touch cell. Skipping']) 
     end
     
+    pause 
+%   if i ==29  
+      saveDir = 'C:\Users\jacheung\Dropbox\LocationCode\Figures\Parts\Fig3\';
+      fn = ['angle_X_curvature_ex2.eps'];
+      export_fig([saveDir, fn], '-depsc', '-painters', '-r1200', '-transparent')
+      fix_eps_fonts([saveDir, fn])
+%   end
 end
 %% analyses
+% peak vs non peak 
+loc_diff = dk_peak_diff(loc_units,:);
+c_diff = loc_diff(~isnan(loc_diff(:,1)),:);
+norm_c_diff = c_diff./max(c_diff,[],2);
+figure(23040);clf
+plot(norm_c_diff','o-', 'color',[.8 .8 .8])
+hold on; errorbar([1 2],mean(norm_c_diff),std(norm_c_diff),'bo-')
+set(gca,'xlim',[0.5 2.5])
+
+
 %analyzing average correlation between high and low dk
 nonempty = ccoeff(~cellfun(@isempty, ccoeff));
 c_accum = zeros(size(nonempty{1}));
@@ -120,7 +208,10 @@ title('on average, higher dk leads to increased firing rates')
 % testing to see if rate of change from one position is sig diff from another
 anova1(cell2mat(fr_diff)')
 
-
+saveDir = 'C:\Users\jacheung\Dropbox\LocationCode\Figures\Parts\Fig3\';
+fn = ['angle_X_curvature_diff.eps'];
+export_fig([saveDir, fn], '-depsc', '-painters', '-r1200', '-transparent')
+fix_eps_fonts([saveDir, fn])
 
 %% simple glm for touch cells only
 fitCoeffs = cell(1,numel(U));
